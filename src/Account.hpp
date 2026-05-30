aac71dfafcdbede537c987bea6b08ac48c8ad609
ChefBuchta <martin.horak.459@gmail.com>
2026-05-30 10:13:02 +0200

    Account: made basic account class

--- src/Account.hpp
+++ src/Account.hpp
@@ -6,8 +6,14 @@ class Account{
   int balance_;
 
   public:
-  Account(const std::string& IBAN, int stBalance);
-  int getBalance() const;
-  int getIBAN() const;
-  int changeBalance(int amount); 
+  Account(const std::string& IBAN, int stBalance): IBAN_(IBAN), balance_(stBalance){}
+  int getBalance() const{
+    return balance_;
+  }
+  std::string getIBAN() const{
+    return IBAN_;
+  }
+  int changeBalance(int amount){
+    return balance_ += amount;  
+  }
 };
